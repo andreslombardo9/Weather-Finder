b1b721c76d2dc94af89c8d10b578e3e7edce5960
andres <andres.lombardo.96@hotmail.com>
2023-05-15 19:24:28 -0300

    Primer commit Widget de Clima

---
+++ src/components/weatherApp.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import WeatherForm from "./weatherForm";
+import WeatherMainInfo from "./weatherMaininfo";
+import styles from './weatherApp.module.css'
+import Loading from './loading'
+
+
+export default function WeatherApp() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    loadInfo();
+  }, []);
+
+  useEffect(() => {
+    document.title = `Weather | ${weather?.location.name ?? ""}`;
+  }, [weather]);
+
+  async function loadInfo(city = "argentina") {
+
+    try {
+      const request = await fetch(
+        `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+      );
+      const json = await request.json();
+      console.log(json);
+
+      setTimeout(() => {
+        setWeather({ ...json });
+      },2000);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function handleOnChangeCity(city) {
+    setWeather(null);
+    loadInfo(city);
+  }
+
+  return (
+    <div className={styles.weatherContainer}>
+      <WeatherForm onChangeCity={handleOnChangeCity} />
+      {weather ? <WeatherMainInfo weather={weather}/> : <Loading/>}
+    </div>
+  );
+}
